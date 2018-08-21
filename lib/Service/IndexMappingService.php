1680df66eeb430d97ea4b1542423449c3cd6bc44
Maxence Lange <maxence@artificial-owl.com>
2018-08-21 17:28:23 +0200

    assign pipeline only if content exist

--- lib/Service/IndexMappingService.php
+++ lib/Service/IndexMappingService.php
@@ -136,7 +136,7 @@ class IndexMappingService {
 	 * @param array $arr
 	 */
 	public function onIndexingDocument(IndexDocument $document, &$arr) {
-		if ($document->isContentEncoded() === IndexDocument::ENCODED_BASE64) {
+		if ($document->getContent() !== '' && $document->isContentEncoded() === IndexDocument::ENCODED_BASE64) {
 			$arr['index']['pipeline'] = 'attachment';
 		}
 	}
