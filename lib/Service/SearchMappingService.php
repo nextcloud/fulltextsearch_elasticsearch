4b8b36ec4ef6784b64d962fc491d23bbe9771c4f
Maxence Lange <maxence@artificial-owl.com>
2020-01-09 17:02:35 -0100

    +wildcard

--- lib/Service/SearchMappingService.php
+++ lib/Service/SearchMappingService.php
@@ -386,6 +386,11 @@ class SearchMappingService {
 				$simpleQuery[] = ['term' => [$query->getField() => $value]];
 			}
 
+			if ($query->getType() === ISearchRequestSimpleQuery::COMPARE_TYPE_WILDCARD) {
+				$value = $query->getValues()[0];
+				$simpleQuery[] = ['wildcard' => [$query->getField() => $value]];
+			}
+
 			if ($query->getType() === ISearchRequestSimpleQuery::COMPARE_TYPE_INT_EQ) {
 				$value = $query->getValues()[0];
 				$simpleQuery[] = ['term' => [$query->getField() => $value]];
