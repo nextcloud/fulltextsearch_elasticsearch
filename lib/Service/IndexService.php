c8f23e1c43918722d7fcc1caf7965262eaf3361e
Maxence Lange <maxence@artificial-owl.com>
2021-08-10 11:46:40 -0100

    lib replacement
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Service/IndexService.php
+++ lib/Service/IndexService.php
@@ -31,7 +31,7 @@ declare(strict_types=1);
 namespace OCA\FullTextSearch_Elasticsearch\Service;
 
 
-use daita\MySmallPhpTools\Traits\TArrayTools;
+use ArtificialOwl\MySmallPhpTools\Traits\TArrayTools;
 use Elasticsearch\Client;
 use Elasticsearch\Common\Exceptions\BadRequest400Exception;
 use Elasticsearch\Common\Exceptions\Missing404Exception;
