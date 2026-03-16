7a5be8ebf430aaaf4fbe964e6232e8c10a6c4d0a
Maxence Lange <maxence@artificial-owl.com>
2026-03-16 20:45:01 -0100

    feat(certificates): use core ca bundle
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    
    [skip ci]

--- lib/Platform/ElasticSearchPlatform.php
+++ lib/Platform/ElasticSearchPlatform.php
@@ -49,6 +49,7 @@ use OCP\FullTextSearch\Model\IIndex;
 use OCP\FullTextSearch\Model\IIndexDocument;
 use OCP\FullTextSearch\Model\IRunner;
 use OCP\FullTextSearch\Model\ISearchResult;
+use OCP\ICertificateManager;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -68,6 +69,7 @@ class ElasticSearchPlatform implements IFullTextSearchPlatform {
 		private ConfigService $configService,
 		private IndexService $indexService,
 		private SearchService $searchService,
+        private ICertificateManager $certificateManager,
 		private LoggerInterface $logger,
 	) {
 	}
