moskvin/files_fulltextsearch

Fix admin UI without global jQuery
Signed-off-by: Niko M <nikolay.moskvin@gmail.com>

js/admin.js
@@ -7,22 +7,31 @@
 /** global: files_elements */
 /** global: files_settings */
 
+function ready(callback) {
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', callback);
+	} else {
+		callback();
+	}
+}
 
-$(document).ready(function () {
+
+ready(function () {
 
 
 	/**
 	 * @constructs Fts_Files
 	 */
 	var Fts_Files = function () {
-		$.extend(Fts_Files.prototype, files_elements);
-		$.extend(Fts_Files.prototype, files_settings);
+		Object.assign(Fts_Files.prototype, files_elements, files_settings);
 
 		files_elements.init();
 		files_settings.refreshSettingPage();
 	};
 
-	OCA.FullTextSearchAdmin.files = Fts_Files;
-	OCA.FullTextSearchAdmin.files.settings = new Fts_Files();
+	window.OCA = window.OCA || {};
+	window.OCA.FullTextSearchAdmin = window.OCA.FullTextSearchAdmin || {};
+	window.OCA.FullTextSearchAdmin.files = Fts_Files;
+	window.OCA.FullTextSearchAdmin.files.settings = new Fts_Files();
 
 });

js/admin.settings.js
@@ -15,23 +15,20 @@ var files_settings = {
 
 	refreshSettingPage: function () {
 
-		$.ajax({
-			method: 'GET',
-			url: OC.generateUrl('/apps/files_fulltextsearch/admin/settings')
-		}).done(function (res) {
+		files_settings.request('GET', '/apps/files_fulltextsearch/admin/settings').then(function (res) {
 			files_settings.updateSettingPage(res);
 		});
 
 	},
 
 	updateSettingPage: function (result) {
-		files_elements.files_local.prop('checked', (result.files_local === true));
-		files_elements.files_external.val(result.files_external);
-		files_elements.files_group_folders.prop('checked', (result.files_group_folders === true));
-		files_elements.files_size.val(result.files_size);
-		files_elements.files_office.prop('checked', (result.files_office === true));
-		files_elements.files_pdf.prop('checked', (result.files_pdf === true));
-    	files_elements.files_open_result_directly.prop('checked', (result.files_open_result_directly === true));
+		files_elements.files_local.checked = (result.files_local === true);
+		files_elements.files_external.value = result.files_external;
+		files_elements.files_group_folders.checked = (result.files_group_folders === true);
+		files_elements.files_size.value = result.files_size;
+		files_elements.files_office.checked = (result.files_office === true);
+		files_elements.files_pdf.checked = (result.files_pdf === true);
+		files_elements.files_open_result_directly.checked = (result.files_open_result_directly === true);
 
 		fts_admin_settings.tagSettingsAsSaved(files_elements.files_div);
 	},
@@ -39,24 +36,53 @@ var files_settings = {
 
 	saveSettings: function () {
 		var data = {
-			files_local: (files_elements.files_local.is(':checked')) ? 1 : 0,
-			files_external: files_elements.files_external.val(),
-			files_group_folders: (files_elements.files_group_folders.is(':checked')) ? 1 : 0,
-			files_size: files_elements.files_size.val(),
-			files_office: (files_elements.files_office.is(':checked')) ? 1 : 0,
-			files_pdf: (files_elements.files_pdf.is(':checked')) ? 1 : 0,
-			files_open_result_directly: (files_elements.files_open_result_directly.is(':checked')) ? 1 : 0
+			files_local: files_elements.files_local.checked ? 1 : 0,
+			files_external: files_elements.files_external.value,
+			files_group_folders: files_elements.files_group_folders.checked ? 1 : 0,
+			files_size: files_elements.files_size.value,
+			files_office: files_elements.files_office.checked ? 1 : 0,
+			files_pdf: files_elements.files_pdf.checked ? 1 : 0,
+			files_open_result_directly: files_elements.files_open_result_directly.checked ? 1 : 0
 		};
-		$.ajax({
-			method: 'POST',
-			url: OC.generateUrl('/apps/files_fulltextsearch/admin/settings'),
-			data: {
-				data: data
-			}
-		}).done(function (res) {
+		files_settings.request('POST', '/apps/files_fulltextsearch/admin/settings', data).then(function (res) {
 			files_settings.updateSettingPage(res);
 		});
 
+	},
+
+
+	request: function (method, route, data) {
+		var options = {
+			method: method,
+			credentials: 'same-origin',
+			headers: {
+				'Accept': 'application/json',
+				'requesttoken': window.OC ? window.OC.requestToken : ''
+			}
+		};
+
+		if (method === 'POST') {
+			options.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+			options.body = files_settings.encodeData(data);
+		}
+
+		return fetch(window.OC.generateUrl(route), options).then(function (response) {
+			if (!response.ok) {
+				throw new Error('Request failed: ' + response.status);
+			}
+
+			return response.json();
+		});
+	},
+
+
+	encodeData: function (data) {
+		var params = new URLSearchParams();
+		Object.keys(data).forEach(function (key) {
+			params.append('data[' + key + ']', data[key]);
+		});
+
+		return params.toString();
 	}
 
 

nextcloud/fulltextsearch_elasticsearch

js/admin.js
@@ -7,22 +7,31 @@
 /** global: elasticsearch_elements */
 /** global: elasticsearch_settings */
 
+function ready(callback) {
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', callback);
+	} else {
+		callback();
+	}
+}
 
-$(document).ready(function () {
+
+ready(function () {
 
 
 	/**
 	 * @constructs ElasticSearchAdmin
 	 */
 	var ElasticSearchAdmin = function () {
-		$.extend(ElasticSearchAdmin.prototype, elasticsearch_elements);
-		$.extend(ElasticSearchAdmin.prototype, elasticsearch_settings);
+		Object.assign(ElasticSearchAdmin.prototype, elasticsearch_elements, elasticsearch_settings);
 
 		elasticsearch_elements.init();
 		elasticsearch_settings.refreshSettingPage();
 	};
 
-	OCA.FullTextSearchAdmin.elasticSearch = ElasticSearchAdmin;
-	OCA.FullTextSearchAdmin.elasticSearch.settings = new ElasticSearchAdmin();
+	window.OCA = window.OCA || {};
+	window.OCA.FullTextSearchAdmin = window.OCA.FullTextSearchAdmin || {};
+	window.OCA.FullTextSearchAdmin.elasticSearch = ElasticSearchAdmin;
+	window.OCA.FullTextSearchAdmin.elasticSearch.settings = new ElasticSearchAdmin();
 
 });

js/admin.settings.js
@@ -16,10 +16,7 @@ var elasticsearch_settings = {
 
 	refreshSettingPage: function () {
 
-		$.ajax({
-			method: 'GET',
-			url: OC.generateUrl('/apps/fulltextsearch_elasticsearch/admin/settings')
-		}).done(function (res) {
+		elasticsearch_settings.request('GET', '/apps/fulltextsearch_elasticsearch/admin/settings').then(function (res) {
 			elasticsearch_settings.updateSettingPage(res);
 		});
 
@@ -29,9 +26,9 @@ var elasticsearch_settings = {
 	/** @namespace result.elastic_index */
 	updateSettingPage: function (result) {
 
-		elasticsearch_elements.elasticsearch_host.val(result.elastic_host);
-		elasticsearch_elements.elasticsearch_index.val(result.elastic_index);
-		elasticsearch_elements.analyzer_tokenizer.val(result.analyzer_tokenizer);
+		elasticsearch_elements.elasticsearch_host.value = result.elastic_host;
+		elasticsearch_elements.elasticsearch_index.value = result.elastic_index;
+		elasticsearch_elements.analyzer_tokenizer.value = result.analyzer_tokenizer;
 
 		fts_admin_settings.tagSettingsAsSaved(elasticsearch_elements.elasticsearch_div);
 	},
@@ -40,21 +37,50 @@ var elasticsearch_settings = {
 	saveSettings: function () {
 
 		var data = {
-			elastic_host: elasticsearch_elements.elasticsearch_host.val(),
-			elastic_index: elasticsearch_elements.elasticsearch_index.val(),
-			analyzer_tokenizer: elasticsearch_elements.analyzer_tokenizer.val()
+			elastic_host: elasticsearch_elements.elasticsearch_host.value,
+			elastic_index: elasticsearch_elements.elasticsearch_index.value,
+			analyzer_tokenizer: elasticsearch_elements.analyzer_tokenizer.value
 		};
 
-		$.ajax({
-			method: 'POST',
-			url: OC.generateUrl('/apps/fulltextsearch_elasticsearch/admin/settings'),
-			data: {
-				data: data
-			}
-		}).done(function (res) {
+		elasticsearch_settings.request('POST', '/apps/fulltextsearch_elasticsearch/admin/settings', data).then(function (res) {
 			elasticsearch_settings.updateSettingPage(res);
 		});
 
+	},
+
+
+	request: function (method, route, data) {
+		var options = {
+			method: method,
+			credentials: 'same-origin',
+			headers: {
+				'Accept': 'application/json',
+				'requesttoken': window.OC ? window.OC.requestToken : ''
+			}
+		};
+
+		if (method === 'POST') {
+			options.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+			options.body = elasticsearch_settings.encodeData(data);
+		}
+
+		return fetch(window.OC.generateUrl(route), options).then(function (response) {
+			if (!response.ok) {
+				throw new Error('Request failed: ' + response.status);
+			}
+
+			return response.json();
+		});
+	},
+
+
+	encodeData: function (data) {
+		var params = new URLSearchParams();
+		Object.keys(data).forEach(function (key) {
+			params.append('data[' + key + ']', data[key]);
+		});
+
+		return params.toString();
 	}
 
 
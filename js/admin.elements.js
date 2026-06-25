c89ff10ec3ed2b0394a05225e0cfb117606687d8
Niko M <nikolay.moskvin@gmail.com>
2026-06-25 12:53:54 +1000

    Fix admin UI without global jQuery
    Signed-off-by: Niko M <nikolay.moskvin@gmail.com>

--- js/admin.elements.js
+++ js/admin.elements.js
@@ -16,31 +16,24 @@ var elasticsearch_elements = {
 
 
 	init: function () {
-		elasticsearch_elements.elasticsearch_div = $('#elastic_search');
-		elasticsearch_elements.elasticsearch_host = $('#elasticsearch_host');
-		elasticsearch_elements.elasticsearch_index = $('#elasticsearch_index');
-		elasticsearch_elements.analyzer_tokenizer = $('#analyzer_tokenizer');
+		elasticsearch_elements.elasticsearch_div = document.getElementById('elastic_search');
+		elasticsearch_elements.elasticsearch_host = document.getElementById('elasticsearch_host');
+		elasticsearch_elements.elasticsearch_index = document.getElementById('elasticsearch_index');
+		elasticsearch_elements.analyzer_tokenizer = document.getElementById('analyzer_tokenizer');
 
-		elasticsearch_elements.elasticsearch_host.on('input', function () {
-			fts_admin_settings.tagSettingsAsNotSaved($(this));
-		}).blur(function () {
-			elasticsearch_settings.saveSettings();
+		elasticsearch_elements.bindInput(elasticsearch_elements.elasticsearch_host);
+		elasticsearch_elements.bindInput(elasticsearch_elements.elasticsearch_index);
+		elasticsearch_elements.bindInput(elasticsearch_elements.analyzer_tokenizer);
+	},
+
+
+	bindInput: function (element) {
+		element.addEventListener('input', function () {
+			fts_admin_settings.tagSettingsAsNotSaved(this);
 		});
-
-		elasticsearch_elements.elasticsearch_index.on('input', function () {
-			fts_admin_settings.tagSettingsAsNotSaved($(this));
-		}).blur(function () {
-			elasticsearch_settings.saveSettings();
-		});
-
-		elasticsearch_elements.analyzer_tokenizer.on('input', function () {
-			fts_admin_settings.tagSettingsAsNotSaved($(this));
-		}).blur(function () {
+		element.addEventListener('blur', function () {
 			elasticsearch_settings.saveSettings();
 		});
 	}
-
-
 };
-
 
