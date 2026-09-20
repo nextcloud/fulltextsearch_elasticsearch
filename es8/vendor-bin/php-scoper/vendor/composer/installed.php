b885ba28e3140ab4aa6ddc1d1472773fad7ef7f8
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-20 07:29:57 +0000

    chore(dev-deps): Bump nextcloud/ocp package
    Signed-off-by: GitHub <noreply@github.com>

--- es8/vendor-bin/php-scoper/vendor/composer/installed.php
+++ es8/vendor-bin/php-scoper/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-stable32',
         'version' => 'dev-stable32',
-        'reference' => '15ada8d7b8292fd18cf091c529e75f3a336340e5',
+        'reference' => 'f47cdd4ad9ddef412d9fcf45d8758889a5b07a9a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-stable32',
             'version' => 'dev-stable32',
-            'reference' => '15ada8d7b8292fd18cf091c529e75f3a336340e5',
+            'reference' => 'f47cdd4ad9ddef412d9fcf45d8758889a5b07a9a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
